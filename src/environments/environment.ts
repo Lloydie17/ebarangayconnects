63bd748a3154bda7b1651eed7c0f2d3b03268f19
Lloydie17 <johnlloydmiral17@gmail.com>
2024-11-13 20:59:54 +0800

    hays

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'https://ebarangayconnect-server.onrender.com/'
+    apiUrl: 'https://ebarangayconnect-server.onrender.com'
   };
   
   /*
